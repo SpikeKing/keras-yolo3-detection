546988c48e74f0e31784ce25fb78d4d010bea611
wcl1 <wcl1@meitu.com>
2018-07-04 15:53:46 +0800

    update

--- yolo3_predict.py
+++ yolo3_predict.py
@@ -56,51 +56,44 @@ class YOLO(object):
         model_path = os.path.expanduser(self.model_path)  # 转换~
         assert model_path.endswith('.h5'), 'Keras model or weights must be a .h5 file.'
 
-        # Load model, or construct model and load weights.
         num_anchors = len(self.anchors)  # anchors的数量
         num_classes = len(self.class_names)  # 类别数
 
+        # 加载模型参数
         self.yolo_model = yolo_body(Input(shape=(None, None, 3)), 3, num_classes)
         self.yolo_model.load_weights(model_path)
 
-        print('{} model, anchors, and classes loaded.'.format(model_path))
+        print('{} model, {} anchors, and {} classes loaded.'.format(model_path, num_anchors, num_classes))
 
-        # Generate colors for drawing bounding boxes.
+        # 不同的框，不同的颜色
         hsv_tuples = [(float(x) / len(self.class_names), 1., 1.)
                       for x in range(len(self.class_names))]  # 不同颜色
         self.colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
-        self.colors = list(
-            map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)),
-                self.colors))  # 转换为真实的颜色
+        self.colors = list(map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), self.colors))  # RGB
+        np.random.seed(10101)
+        np.random.shuffle(self.colors)
+        np.random.seed(None)
 
-        # 设置随机数颜色
-        np.random.seed(10101)  # Fixed seed for consistent colors across runs.
-        np.random.shuffle(self.colors)  # Shuffle colors to decorrelate adjacent classes.
-        np.random.seed(None)  # Reset seed to default.
-
-        # Generate output tensor targets for filtered bounding boxes.
+        # 根据检测参数，过滤框
         self.input_image_shape = K.placeholder(shape=(2,))
-        boxes, scores, classes = yolo_eval(self.yolo_model.output, self.anchors,
-                                           len(self.class_names), self.input_image_shape,
-                                           score_threshold=self.score, iou_threshold=self.iou)
+        boxes, scores, classes = yolo_eval(self.yolo_model.output, self.anchors, len(self.class_names),
+                                           self.input_image_shape, score_threshold=self.score, iou_threshold=self.iou)
         return boxes, scores, classes
 
     def detect_image(self, image):
         start = timer()  # 起始时间
 
-        if self.model_image_size != (None, None):  # 416x416, 32*13
+        if self.model_image_size != (None, None):  # 416x416, 416=32*13，必须为32的倍数，最小尺度是除以32
             assert self.model_image_size[0] % 32 == 0, 'Multiples of 32 required'
             assert self.model_image_size[1] % 32 == 0, 'Multiples of 32 required'
-            boxed_image = letterbox_image(image, tuple(reversed(self.model_image_size)))
+            boxed_image = letterbox_image(image, tuple(reversed(self.model_image_size)))  # 填充图像
         else:
-            new_image_size = (image.width - (image.width % 32),
-                              image.height - (image.height % 32))
+            new_image_size = (image.width - (image.width % 32), image.height - (image.height % 32))
             boxed_image = letterbox_image(image, new_image_size)
         image_data = np.array(boxed_image, dtype='float32')
-
-        print(image_data.shape)
+        print('detector size {}'.format(image_data.shape))
         image_data /= 255.  # 转换0~1
-        image_data = np.expand_dims(image_data, 0)  # Add batch dimension.  # 增加批次维度
+        image_data = np.expand_dims(image_data, 0)  # 添加批次维度，将图片增加1维
 
         # 参数盒子、得分、类别；输入图像0~1，4维；原始图像的尺寸
         out_boxes, out_scores, out_classes = self.sess.run(
@@ -114,39 +107,38 @@ class YOLO(object):
         print('Found {} boxes for {}'.format(len(out_boxes), 'img'))  # 检测出的框
 
         font = ImageFont.truetype(font='font/FiraMono-Medium.otf',
-                                  size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
-        thickness = (image.size[0] + image.size[1]) // 300
-
+                                  size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))  # 字体
+        thickness = (image.size[0] + image.size[1]) // 512  # 厚度
         for i, c in reversed(list(enumerate(out_classes))):
-            predicted_class = self.class_names[c]
-            box = out_boxes[i]
-            score = out_scores[i]
+            predicted_class = self.class_names[c]  # 类别
+            box = out_boxes[i]  # 框
+            score = out_scores[i]  # 执行度
 
-            label = '{} {:.2f}'.format(predicted_class, score)
-            draw = ImageDraw.Draw(image)
-            label_size = draw.textsize(label, font)
+            label = '{} {:.2f}'.format(predicted_class, score)  # 标签
+            draw = ImageDraw.Draw(image)  # 画图
+            label_size = draw.textsize(label, font)  # 标签文字
 
             top, left, bottom, right = box
             top = max(0, np.floor(top + 0.5).astype('int32'))
             left = max(0, np.floor(left + 0.5).astype('int32'))
             bottom = min(image.size[1], np.floor(bottom + 0.5).astype('int32'))
             right = min(image.size[0], np.floor(right + 0.5).astype('int32'))
-            print(label, (left, top), (right, bottom))
+            print(label, (left, top), (right, bottom))  # 边框
 
-            if top - label_size[1] >= 0:
+            if top - label_size[1] >= 0:  # 标签文字
                 text_origin = np.array([left, top - label_size[1]])
             else:
                 text_origin = np.array([left, top + 1])
 
             # My kingdom for a good redistributable image drawing library.
-            for i in range(thickness):
+            for i in range(thickness):  # 画框
                 draw.rectangle(
                     [left + i, top + i, right - i, bottom - i],
                     outline=self.colors[c])
-            draw.rectangle(
+            draw.rectangle(  # 文字背景
                 [tuple(text_origin), tuple(text_origin + label_size)],
                 fill=self.colors[c])
-            draw.text(text_origin, label, fill=(0, 0, 0), font=font)
+            draw.text(text_origin, label, fill=(0, 0, 0), font=font)  # 文案
             del draw
 
         end = timer()
@@ -157,69 +149,14 @@ class YOLO(object):
         self.sess.close()
 
 
-def detect_video(yolo, video_path, output_path=""):
-    import cv2
-    vid = cv2.VideoCapture(video_path)
-    if not vid.isOpened():
-        raise IOError("Couldn't open webcam or video")
-    video_FourCC = int(vid.get(cv2.CAP_PROP_FOURCC))
-    video_fps = vid.get(cv2.CAP_PROP_FPS)
-    video_size = (int(vid.get(cv2.CAP_PROP_FRAME_WIDTH)),
-                  int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-    isOutput = True if output_path != "" else False
-    if isOutput:
-        print("!!! TYPE:", type(output_path), type(video_FourCC), type(video_fps), type(video_size))
-        out = cv2.VideoWriter(output_path, video_FourCC, video_fps, video_size)
-    accum_time = 0
-    curr_fps = 0
-    fps = "FPS: ??"
-    prev_time = timer()
-    while True:
-        return_value, frame = vid.read()
-        image = Image.fromarray(frame)
-        image = yolo.detect_image(image)
-        result = np.asarray(image)
-        curr_time = timer()
-        exec_time = curr_time - prev_time
-        prev_time = curr_time
-        accum_time = accum_time + exec_time
-        curr_fps = curr_fps + 1
-        if accum_time > 1:
-            accum_time = accum_time - 1
-            fps = "FPS: " + str(curr_fps)
-            curr_fps = 0
-        cv2.putText(result, text=fps, org=(3, 15), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                    fontScale=0.50, color=(255, 0, 0), thickness=1)
-        cv2.namedWindow("result", cv2.WINDOW_NORMAL)
-        cv2.imshow("result", result)
-        if isOutput:
-            out.write(result)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    yolo.close_session()
-
-
-def detect_img(yolo):
-    while True:
-        img = input('Input image filename:')
-        try:
-            image = Image.open(img)
-        except:
-            print('Open Error! Try again!')
-            continue
-        else:
-            r_image = yolo.detect_image(image)
-            r_image.show()
-    yolo.close_session()
-
-
 def detect_img_for_test(yolo):
     img_path = './dataset/a4386X6Te9ajq866zgOtWKLx18XGW.jpg'
     image = Image.open(img_path)
     r_image = yolo.detect_image(image)
     r_image.show()
+    r_image.save(img_path + '.d.jpg')
+    yolo.close_session()
 
 
 if __name__ == '__main__':
-    # detect_img(YOLO())
     detect_img_for_test(YOLO())
