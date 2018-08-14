8957fc5d6a2d92d8f2c1e39b8ba65ff8b10ea2d7
wcl1 <wcl1@meitu.com>
2018-08-14 17:20:38 +0800

    update

--- test.py
+++ test.py
@@ -7,5 +7,14 @@ Created by C. L. Wang on 2018/8/2
 
 import numpy as np
 
-a = np.array([1, 2, 3, 4, 5])
-print a[::-1]
+a = np.array([[1, 2]])
+print(a.shape)  # (1, 2)
+b = np.array([[1], [2]])
+print(b.shape)  # (2, 1)
+c = a + b
+print(c.shape)  # (2, 2)
+print(c)
+"""
+[[2 3]
+ [3 4]]
+"""
